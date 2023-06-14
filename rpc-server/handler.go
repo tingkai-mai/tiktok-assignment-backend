9f1104847de1e73acd90bd13f259195a79b6c61f
Ferusel <53888954+Ferusel@users.noreply.github.com>
2023-06-14 15:14:47 +0800

    Add send

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
 	"math/rand"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
-
 // IMServiceImpl implements the last service interface defined in the IDL.
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
+	if err := validateSendRequest(req); err != nil {
+		return nil, err
+	}
+
+	timestamp := time.Now().Unix()
+	message := &Message{
+		Message:   req.Message.GetText(),
+		Sender:    req.Message.GetSender(),
+		Timestamp: timestamp,
+	}
+
+	roomID, err := getRoomID(req.Message.GetChat())
+	if err != nil {
+		return nil, err
+	}
+
+	err = rdb.SaveMessage(ctx, roomID, message)
+	if err != nil {
+		return nil, err
+	}
+
 	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Code, resp.Msg = 0, "success"
 	return resp, nil
 }
 
@@ -28,4 +51,41 @@ func areYouLucky() (int32, string) {
 	} else {
 		return 500, "oops"
 	}
+}
+
+func validateSendRequest(req *rpc.SendRequest) error {
+	senders := strings.Split(req.Message.Chat, ":")
+	if len(senders) != 2 {
+		err := fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", req.Message.GetChat())
+		return err
+	}
+	sender1, sender2 := senders[0], senders[1]
+
+	if req.Message.GetSender() != sender1 && req.Message.GetSender() != sender2 {
+		err := fmt.Errorf("sender '%s' not in the chat room", req.Message.GetSender())
+		return err
+	}
+
+	return nil
+}
+
+func getRoomID(chat string) (string, error) {
+	var roomID string
+
+	lowercase := strings.ToLower(chat)
+	senders := strings.Split(lowercase, ":")
+	if len(senders) != 2 {
+		err := fmt.Errorf("invalid Chat ID '%s', should be in the format of user1:user2", chat)
+		return "", err
+	}
+
+	sender1, sender2 := senders[0], senders[1]
+	// Compare the sender and receiver alphabetically, and sort it asc to form the room ID
+	if comp := strings.Compare(sender1, sender2); comp == 1 {
+		roomID = fmt.Sprintf("%s:%s", sender2, sender1)
+	} else {
+		roomID = fmt.Sprintf("%s:%s", sender1, sender2)
+	}
+
+	return roomID, nil
 }
